7ecec64794676c1ca647c50b3d12f3bbf95d4dce
huynhphong155 <huynhphongbmt6969@gmail.com>
2022-01-26 18:31:16 +0700

    add code

--- reacts-project2/src/utils/schema/index.js
+++
@@ -1,6 +0,0 @@
-import ListOrder from "./ListOrder";
-import OrderDetail from "./OrderDetail";
-import ListBilling from "./ListBilling";
-import ListPaymentOrder from "./ListPaymentOrder";
-
-export { ListOrder, OrderDetail, ListBilling, ListPaymentOrder };
