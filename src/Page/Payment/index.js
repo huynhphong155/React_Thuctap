bd8c50f13800933b5e4cbb02e35e70924303c0c8
huynhphong155 <huynhphongbmt6969@gmail.com>
2022-02-20 22:44:58 +0700

    add code

--- src/Page/Payment/index.js
+++ src/Page/Payment/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import RenderUI from "../../HOC/RenderUI";
 
-function tracuu() {
+function thanhtoan() {
   return (
     <RenderUI>
       <div>
-        <h1>Tra cứu công nợ</h1>
+        <h1>THANH TOÁN TRỰC TUYẾN</h1>
       </div>
     </RenderUI>
   );
 }
 
-export default tracuu;
+export default thanhtoan;
